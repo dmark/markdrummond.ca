194d95b88e7b9bf66e937e9c195fed3526eedaa9
Mark Drummond <dmark@users.noreply.github.com>
2026-08-30 12:50:09 -0400

    Quartz sync: Aug 30, 2026, 12:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,7 +29,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f5",
+          light: "#b7a17b",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
